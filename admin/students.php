e42745bf6338c1fc26985a78f4e97041430a7cac
Shakayet Hossain Redoy <59065@edu.nielsbrock.dk>
2026-03-12 13:25:26 +0100

    feat: Add  code every admin file

--- admin/students.php
+++ admin/students.php
@@ -1,1 +1,8 @@
-<?php //admin/students.php
+<?php
+session_start();
+require_once '../includes/auth.php';
+requireAdmin();                          // Redirects to login if not logged in
+require_once '../includes/db.php';
+require_once '../includes/helpers.php';
+require_once '../includes/header.php';
+// ... rest of your code below
