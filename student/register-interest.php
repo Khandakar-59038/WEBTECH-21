3a6daec323761a9969af0b6bc76329aaecd7ec78
Iftisam Sarwar Ushna <59035@edu.nielsbrock.dk>
2026-03-16 14:28:49 +0100

    feat: Add register-interest.php with POST-only check

--- student/register-interest.php
+++ student/register-interest.php
@@ -1,1 +1,11 @@
-<?php //student/register-interest.php
+<?php
+// student/register-interest.php --- Process the interest registration form
+// CTEC2712N --- Ushno
+session_start();
+require_once '../includes/db.php';
+require_once '../includes/helpers.php';
+
+// Only accept POST requests --- reject anything else
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    redirect('index.php');
+}
